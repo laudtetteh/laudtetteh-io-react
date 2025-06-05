ce84fc4b62a1d026a2cd0fa2f2beeb56b6b82838
Laud Tetteh (Beacon Avenue) <laud@studiotenfour.com>
2025-06-05 01:51:47 -0700

    fix: @ path alias - 4th try

--- pages/admin/create.tsx
+++ pages/admin/create.tsx
@@ -1,7 +1,6 @@
-import useAuthRedirect from "@/lib/useAuthRedirect";
-
 import { useRouter } from "next/router";
 import AdminPostForm from "@/components/AdminPostForm";
+import useAuthRedirect from "@/lib/useAuthRedirect";
 
 export default function CreatePostPage() {
   useAuthRedirect();
