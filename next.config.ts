ebce10f76753859154213aea3dda51aa2e2e78f0
Laud Tetteh (Beacon Avenue) <laud@studiotenfour.com>
2025-06-05 01:35:00 -0700

    fix: @ path alias

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -6,6 +7,14 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_IS_DOCKER: process.env.NEXT_PUBLIC_IS_DOCKER,
     NEXT_PUBLIC_API_LOCAL: process.env.NEXT_PUBLIC_API_LOCAL,
     NEXT_PUBLIC_API_DOCKER: process.env.NEXT_PUBLIC_API_DOCKER,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
   },
 };
 
